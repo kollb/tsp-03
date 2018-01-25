aa32f423f91d752cb6fa8746a94c9dc359c3ca6a
kollb <dermont@web.de>
2018-01-25 16:35:10 +0100

    initial commit

--- src/mutation/DisplacementMutation.java
+++ src/mutation/DisplacementMutation.java
@@ -1,10 +1,36 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import random.MersenneTwisterFast;
+
+import java.util.ArrayList;
 
 public class DisplacementMutation implements IMutation {
     public Tour doMutation(Tour tour) {
-        return null;
+
+        MersenneTwisterFast randomGenerator = new MersenneTwisterFast();
+        int startPoint = randomGenerator.nextInt(tour.getSize());
+        int endPoint = randomGenerator.nextInt(tour.getSize());
+        if(startPoint > endPoint) {
+            int temp = startPoint;
+            startPoint = endPoint;
+            endPoint = temp;
+        }
+        ArrayList<City> cities = tour.getCities();
+        ArrayList<City> displaced = new ArrayList<City>();
+        for(int i = startPoint; i <= endPoint; i++) {
+            displaced.add(cities.remove(i));
+        }
+        int insertionPoint;
+        do{
+            insertionPoint = randomGenerator.nextInt(cities.size());
+        } while(insertionPoint == startPoint);
+        cities.addAll(insertionPoint, displaced);
+
+        tour.setCities(cities);
+
+        return tour;
     }
 
     public String toString() {
