5a81224ee426cba9f57a1fd115adbdfc354ccc5e
Carsten Hagemann <carsten.hagemann@kaufland.com>
2018-01-26 10:07:29 +0100

    Fix broken Crossover file? Parameter names were wrong, imports were missing

--- src/crossover/CycleCrossover.java
+++ src/crossover/CycleCrossover.java
@@ -1,9 +1,11 @@
 package crossover;
 
+import base.City;
 import base.Tour;
+import random.MersenneTwisterFast;
 
 public class CycleCrossover implements ICrossover {
-    public Tour doCrossover(Tour tour01,Tour tour02) {
+    public Tour doCrossover(Tour tour1, Tour tour2) {
         if (tour1.getSize() != tour2.getSize())
             return null;    //error!
 
@@ -48,7 +50,7 @@ public class CycleCrossover implements ICrossover {
                 //search new index
                 index = parent1.getCities().indexOf(currentCity);
 
-            }while(index != start);
+            } while(index != start);
 
         //reset index
             index = 0;
