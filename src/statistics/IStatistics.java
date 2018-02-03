dbe0456e14e5a716451da0950b1ef4142ff38781
JacobNr16 <31903263+JacobNr16@users.noreply.github.com>
2018-02-03 13:10:09 +0100

    Update IStatistics.java

--- src/statistics/IStatistics.java
+++ src/statistics/IStatistics.java
@@ -1,21 +1,13 @@
 package statistics;
 
 public interface IStatistics {
-    void writeCSVFile(String scenarioNumber, Double[] values);
-
+    void writeCSVFile(String scenarioNumber, double[] values);
     void buildMeasureRFile();
-
     void buildBarPlotFile();
-
     void buildBoxPlotRFile();
-
     void buildDotPlotRFile();
-
     void buildHistogramRFile();
-
     void buildStripChartRFile();
-
     void buildTTestRFile();
-
     void buildMostFrequentFitnessValuesRFile();
 }
