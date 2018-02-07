efbfcfd3ed5660dd1ed75b627c1a64eb96421d93
Carsten Hagemann <carsten@chagemann.de>
2018-02-07 19:31:06 +0100

    Remove our names from the files

--- src/selection/PopulationTooSmallException.java
+++ src/selection/PopulationTooSmallException.java
@@ -1,8 +1,5 @@
 package selection;
 
-/**
- * Created by chag0709 on 26.01.18 - Kaufland Informationssysteme 2018.
- */
 public class PopulationTooSmallException extends Exception {
 
     private static final String ERROR = "Tour size is smaller than the configured Select Count.";
