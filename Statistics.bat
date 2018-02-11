277215447375cd84b1c2b1886f346da2276a9cdf
JacobNr16 <31903263+JacobNr16@users.noreply.github.com>
2018-02-11 15:15:09 +0100

    Update Statistics.bat

--- Statistics.bat
+++ Statistics.bat
@@ -1,2 +1,3 @@
 java -classpath build statistics.Statistics -d s1 s2 -m median mean quantile=0.25 quantile=0.25,0.75 quantile=0.25-0.75 range iqr -p bar box dot hist strip -t -a
+echo Die R-Dateien liegen in src/statistics
 pause
